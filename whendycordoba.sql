1583a177d0666eb58d33a4b649e20d3b6c6da694
whendycordoba <36428536+whendycordoba@users.noreply.github.com>
2018-04-25 21:55:10 -0500

    Add files via upload
    export

---
+++ whendycordoba.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+-- Archivo creado  - miércoles-abril-25-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Function FUNCTION_1
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "EJERCICIOS"."FUNCTION_1" (a NUMBER,b  NUMBER,c NUMBER)
+RETURN VARCHAR2 IS
+y VARCHAR2(255);
+BEGIN
+
+IF (b>=c) THEN
+y:='"'||a||'/'||a*b||'/'||'andres'||'"';
+ELSE
+y:='"'||a||'/'||a*b||'/'||'martinez'||'"';
+END IF;
+
+RETURN y;
+END;
